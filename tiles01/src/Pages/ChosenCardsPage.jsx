7d678c0824beabc8440ded23ee44cbd65865ee56
annemoss <anneork@stud.ntnu.no>
2021-02-08 14:49:55 +0100

    multiple links to different categories

--- tiles01/src/Pages/ChosenCardsPage.jsx
+++ tiles01/src/Pages/ChosenCardsPage.jsx
@@ -8,9 +8,21 @@ function ChosenCardsPage() {
             <Link
                 to={{
                     pathname: "/categories",
+                    state: { category: "Trigger" }
+                }}>
+                <p className="Logo">Trigger</p></Link>
+            <Link
+                to={{
+                    pathname: "/categories",
                     state: { category: "Things" }
                 }}>
-                <p className="Logo">Categories</p></Link>
+                <p className="Logo">Things</p></Link>
+            <Link
+                to={{
+                    pathname: "/categories",
+                    state: { category: "Feedback" }
+                }}>
+                <p className="Logo">Feedback</p></Link>
             <Link to="/animation"><p className="Logo">Animation</p></Link>
             <Link to="/"><p className="Logo">Main</p></Link>
         </div>
