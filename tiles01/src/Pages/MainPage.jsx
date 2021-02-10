5aaec8d7eec343868f39cd4b71ab90b5f68b6f66
Emilie Kalleberg <emilie.kalleberg@gmail.com>
2021-02-10 11:29:36 +0100

    added the imgs and placed the imgs on the main page

--- tiles01/src/Pages/MainPage.jsx
+++ tiles01/src/Pages/MainPage.jsx
@@ -3,10 +3,21 @@ import { Link } from "react-router-dom";
 //import './App.css'; 
 //Component imports
 import Logo from "../Components/Logo";
+import motionCard from "../img/triggers/motion.png"
+import colorChangeCard from "../img/feedback/colorChange.png"
+import bikeCard from "../img/things/bike.png"
+import dropCard from "../img/triggers/drop.png"
+
 
 function MainPage() {
+
     return (
         <div className="Page mainPage">
+        <div>
+            <img className="motionCardTop"  alt="The motion card" src={motionCard} />
+            <img className="colorChangeCardTop" alt="ColorChangeCard" src={colorChangeCard}/>
+            <img className="dropCardTop" alt="Drop card" src={dropCard} />
+        </div>
             <Logo/>
             <div className="infoBox">
             <p>Welcome to the Tiles Internet of Things Invention! <br/>
@@ -16,6 +27,11 @@ function MainPage() {
             <Link to="/cards">
             <p>Let's begin!</p>
             </Link>
+            </div>
+            <div>
+                <img className="motionCard"  alt="The motion card" src={motionCard} />
+                <img className="colorChangeCard" alt="ColorChangeCard" src={colorChangeCard}/>
+                <img className="bikeCard" alt="Bike card" src={bikeCard} />
             </div>
 
         </div>
