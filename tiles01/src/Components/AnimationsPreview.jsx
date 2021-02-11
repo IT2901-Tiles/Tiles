5c87fb1b0db1a80adab85b6b83b7c163993e9229
karenvi <karenvi @stud.ntnu.no>
2021-02-11 13:55:20 +0100

    added a new component and a css-file for page

---
+++ tiles01/src/Components/AnimationsPreview.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Component for the preview of the animations in the animation page (final step of the process)
+
+function AnimationsPreview() {
+    return(
+        <div className="animationSquare">
+            Here the animations will be previewed.
+        </div>
+    )
+}
+
+export default AnimationsPreview
--- tiles01/src/Pages/AnimationPage.jsx
+++ tiles01/src/Pages/AnimationPage.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LinkButton from '../Components/LinkButton'
+import AnimationsPreview from "../Components/AnimationsPreview"
+import "../AnimationPage.css" 
 
 function AnimationPage() {
     return (
         <div className="Page">
-            <p>AnimationPage</p>
+            <p>
+                AnimationPage
+            </p>
+                <AnimationsPreview />
             <LinkButton title="Try again!" target="/" size="Large" category="animation"></LinkButton>
-            <p>Test</p>
         </div>
     );
 }
