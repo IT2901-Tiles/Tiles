fc48f7c536d48daae7a5becd514bb552169032b7
karenvi <karenvi @stud.ntnu.no>
2021-02-14 19:34:59 +0100

    nearly done - must pull from #7 to make it work

--- tiles01/src/Components/AnimationsPreview.jsx
+++ tiles01/src/Components/AnimationsPreview.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import LinkButton from './LinkButton';
-import CardContainer from './CardContainer';
-import Card from './Card';
-// Component for the preview of the animations in the animation page (final step of the process)
+// Component for the preview of the animations + the selected cards. 
+// This component uses the grid-property in html and css
 
-function AnimationsPreview(props) {
-
+function AnimationsPreview() {
     return(
         <div className="gridAnimationsPage">
             <div className="gridAnimationItem3">
-                    <div className="card1AnimationsPage">
-                        card placeholder
-                    </div>
-                    <div className="card2AnimationsPage">
-                        card placeholder
-                    </div>
-                    <div className="card3AnimationsPage">
-                        card placeholder
-                    </div>
+                <div className="card1AnimationsPage">
+                    {/* Checking if the user has picked a card or not */}
+                    {localStorage.getItem("trigger") === null ? <p>No "trigger" card chosen.</p> : <img src={localStorage.getItem("trigger")} className="triggerAnimationsPage" alt="Picture of a 'trigger' card" />}
+                </div>
+                <div className="card2AnimationsPage">
+                    {localStorage.getItem("thing") === null ? <p>No "thing" card chosen.</p> : <img src={localStorage.getItem("thing")} className="thingAnimationsPage" alt="Picture of a 'thing' card" />}
+                </div>
+                <div className="card3AnimationsPage">
+                    {localStorage.getItem("feedback") === null ? <p>No "feedback" card chosen.</p> : <img src={localStorage.getItem("feedback")} className="feedbackAnimationsPage" alt="Picture of a 'feedback' card" />}
+                </div>
             </div>
+            {/* The div below is where the animations will appear */}
             <div className="gridAnimationItem4">
                 Area for the animations based off the selected cards
             </div>
+            {/* "Try again!"-button */}
             <div className="gridAnimationItem5">
-            <LinkButton title="Try again!" target="/" size="Large" category="animation"></LinkButton>
+                <LinkButton title="Try again!" target="/" size="Large" category="animation"></LinkButton>
             </div>
         </div>
     )
--- tiles01/src/Pages/AnimationPage.jsx
+++ tiles01/src/Pages/AnimationPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import LinkButton from '../Components/LinkButton'
 import AnimationsPreview from "../Components/AnimationsPreview"
-import "../AnimationPage.css" 
+import "../AnimationPage.css"
 
 function AnimationPage() {
     return (
