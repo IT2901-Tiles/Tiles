cd3f2849e3d0ddbec42efdc67d5d9f6e8d6beab1
millieba <millieba@hotmail.com>
2021-02-15 19:40:41 +0100

    Styled the chosen cards grid

--- tiles01/src/Pages/ChosenCardsPage.jsx
+++ tiles01/src/Pages/ChosenCardsPage.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import { useState } from 'react';
 import LinkButton from '../Components/LinkButton'
 
 function ChosenCardsPage(props) {
-    const [CardTrigger,setCardTrigger] = useState(null);
-    const [CardThing,setCardThing] = useState(null);
-    const [CardFeedback,setCardFeedback] = useState(null);
-    console.log({CardTrigger}, props.location.state.card)
     localStorage.setItem(props.location.state.category,props.location.state.card)
+
     return (
         <div className="Page">
             <p>ChosenCardsPage</p>
-
             <LinkButton target="/categories" title="Trigger" category="trigger" size="Small"></LinkButton>
             <LinkButton target="/categories" title="Things" category="thing" size="Small"></LinkButton>
             <LinkButton target="/categories" title="Feedback" category="feedback" size="Small"></LinkButton>
             <LinkButton target="/animation" title="RUN!" category="animation" size="Large"></LinkButton>
             <LinkButton target="/" title="Back"></LinkButton>
-            <img src={localStorage.getItem("trigger")}/>
-            <img src={localStorage.getItem("thing")}/>
-            <img src={localStorage.getItem("feedback")}/>
+
+            <div>
+                {localStorage.getItem("trigger") === "null" ? <p className="cardNotChosenText">No "trigger" card chosen.</p> : <img src={localStorage.getItem("trigger")} className="chosenCard" alt='Picture of a "trigger" card' />}
+                {localStorage.getItem("thing") === "null" ? <p className="cardNotChosenText">No "thing" card chosen.</p> : <img src={localStorage.getItem("thing")} className="chosenCard" alt='Picture of a "thing" card' />}
+                {localStorage.getItem("feedback") === "null" ? <p className="cardNotChosenText">No "feedback" card chosen.</p> : <img src={localStorage.getItem("feedback")} className="chosenCard" alt='Picture of a "feedback" card' />}
+            </div> 
         </div>
     );
 }
