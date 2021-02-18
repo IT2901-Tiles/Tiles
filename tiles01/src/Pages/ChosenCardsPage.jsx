fcea8f06611acacc45e1c68875c9c0b58f25e878
millieba <millieba@hotmail.com>
2021-02-18 11:59:45 +0100

    Put the chosen cards in a grid

--- tiles01/src/Pages/ChosenCardsPage.jsx
+++ tiles01/src/Pages/ChosenCardsPage.jsx
@@ -13,11 +13,18 @@ function ChosenCardsPage(props) {
             <LinkButton target="/animation" title="RUN!" category="animation" size="Large"></LinkButton>
             <LinkButton target="/" title="Back"></LinkButton>
 
-            <div>
-                {localStorage.getItem("trigger") === "null" ? <p className="cardNotChosenText">No "trigger" card chosen.</p> : <img src={localStorage.getItem("trigger")} className="chosenCard" alt='Picture of a "trigger" card' />}
-                {localStorage.getItem("thing") === "null" ? <p className="cardNotChosenText">No "thing" card chosen.</p> : <img src={localStorage.getItem("thing")} className="chosenCard" alt='Picture of a "thing" card' />}
-                {localStorage.getItem("feedback") === "null" ? <p className="cardNotChosenText">No "feedback" card chosen.</p> : <img src={localStorage.getItem("feedback")} className="chosenCard" alt='Picture of a "feedback" card' />}
-            </div> 
+            {/* Den gamle koden (har ikke problemet med at kortene endrer størrelse når ikke alle kort er valgt) */}
+            {/* <div>
+                {localStorage.getItem("trigger") === "null" ? <p className="cardNotChosenText">No "trigger" card chosen.</p> : <img src={localStorage.getItem("trigger")} className="chosenTrigger" alt='Picture of a "trigger" card' />}
+                {localStorage.getItem("thing") === "null" ? <p className="cardNotChosenText">No "thing" card chosen.</p> : <img src={localStorage.getItem("thing")} className="chosenThing" alt='Picture of a "thing" card' />}
+                {localStorage.getItem("feedback") === "null" ? <p className="cardNotChosenText">No "feedback" card chosen.</p> : <img src={localStorage.getItem("feedback")} className="chosenFeedback" alt='Picture of a "feedback" card' />}
+            </div>  */}
+            
+            <div className="chosenCardsGrid">
+                <div className="triggerDiv">{localStorage.getItem("trigger") === "null" ? <div className="cardNotChosenDiv"><p className="cardNotChosenText">No "trigger" card chosen.</p></div> : <img src={localStorage.getItem("trigger")} className="chosenTrigger" alt='Picture of a "trigger" card' />}</div>
+                <div className="thingDiv">{localStorage.getItem("thing") === "null" ? <p className="cardNotChosenText">No "thing" card chosen.</p> : <img src={localStorage.getItem("thing")} className="chosenThing" alt='Picture of a "thing" card' />}</div>
+                <div className="feedbackDiv">{localStorage.getItem("feedback") === "null" ? <p className="cardNotChosenText">No "feedback" card chosen.</p> : <img src={localStorage.getItem("feedback")} className="chosenFeedback" alt='Picture of a "feedback" card' />}</div>
+            </div>
         </div>
     );
 }
