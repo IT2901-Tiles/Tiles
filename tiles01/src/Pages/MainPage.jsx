58ad64953480cc3447da6110cc17fd34ba8b8bdb
Emilie Kalleberg <emilie.kalleberg@gmail.com>
2021-03-16 10:58:08 +0100

    #26 fix responsiveness on the main page. The card-img will be hidden when on a smart phone

--- tiles01/src/Pages/MainPage.jsx
+++ tiles01/src/Pages/MainPage.jsx
@@ -3,32 +3,30 @@ import React from 'react';
 import Logo from "../Components/Logo";
 import LinkButton from '../Components/LinkButton';
 //img imports
-import motionCard from "./../images/triggers/motion.png"
+/*import motionCard from "./../images/triggers/motion.png"
 import colorChangeCard from "./../images/feedbacks/color_change.png"
 import bikeCard from "./../images/things/bike.png"
-import dropCard from "./../images/triggers/drop.png"
+import dropCard from "./../images/triggers/drop.png"*/
+import stylingImg1 from "./../images/mainPageStyling1.png"
+import stylingImg2 from "./../images/mainPageStyling2.png"
+import img1 from "./../images/img1.png"
+import img2 from "./../images/img2.png"
 
 function MainPage() {
 
     return (
-            <div className="Page mainPage">
-                <div>
-                    <img className="motionCardTop"  alt="The motion card" src={motionCard} />
-                    <img className="colorChangeCardTop" alt="ColorChangeCard" src={colorChangeCard}/>
-                    <img className="dropCardTop" alt="Drop card" src={dropCard} />
-                </div>
-                <Logo/>
-                <div className="infoBox">
-                    <p>Welcome to the Tiles Internet of Things Invention! <br/>
-                    Let’s create some smart creations! </p>
-                </div>
-                <LinkButton title="Let's begin!" target="/cards" size="Large"></LinkButton>
-                <div>
-                    <img className="motionCard"  alt="The motion card" src={motionCard} />
-                    <img className="colorChangeCard" alt="ColorChangeCard" src={colorChangeCard}/>
-                    <img className="bikeCard" alt="Bike card" src={bikeCard} />
-                </div>
+        <div className="Page mainPage">
+            <Logo/>
+            <img className="img2" src={img2} alt="stack of cards2"/>
+            <div className="infoBox">
+                <p>Welcome to the Tiles Internet of Things Invention! <br/>
+                Let’s create some smart creations! </p>
             </div>
+            <LinkButton title="Let's begin!" target="/cards" size="Large"></LinkButton>
+            <div>
+            </div>
+            <img class="mainPage img1" src={img1} alt="stack of cards"/>
+        </div>
     );
 }
 
