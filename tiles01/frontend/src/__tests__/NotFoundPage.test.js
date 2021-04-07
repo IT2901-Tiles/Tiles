0a418e7da02631a0ce10e086b136b2621c5c1dda
Gunvor <gunvorhu@stud.ntnu.no>
2021-04-07 11:59:48 +0200

    #38 added tests to NotFoundPage

--- tiles01/frontend/src/__tests__/NotFoundPage.test.js
+++ tiles01/frontend/src/__tests__/NotFoundPage.test.js
@@ -11,8 +11,25 @@ configure({
     adapter: new Adapter()
 });
 
-describe('Animation page', () => {
+describe('Not Found Page', () => {
+    const wrapper = shallow(<NotFoundPage/>)
     it('renders correctly', () => {
         shallow(<NotFoundPage/>);
-    })
+    });
+    it('renders two LinkButton elements', () => {
+        expect(wrapper.find('LinkButton')).toHaveLength(2);
+    });
+    it('renders the p element correct', () => {
+        const p = wrapper.find('p');
+        expect(p).toHaveLength(1);
+        expect(p.text()).toEqual('It seems like you are a bit lost. Let me help you find your way back! ');
+    });
+    it('renders the h1 element correct', () => {
+        const heading = wrapper.find('h1');
+        expect(heading).toHaveLength(1);
+        expect(heading.text()).toEqual('Page not found');
+    });
+    it('renders eleven images', () => {
+        expect(wrapper.find('img')).toHaveLength(11);
+    });
 });
