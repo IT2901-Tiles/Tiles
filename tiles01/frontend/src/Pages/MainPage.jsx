e6141421c320699debb121f4a881650b3278873d
annemoss <anneork@stud.ntnu.no>
2021-04-15 16:11:08 +0200

    no scroll on main page

--- tiles01/frontend/src/Pages/MainPage.jsx
+++ tiles01/frontend/src/Pages/MainPage.jsx
@@ -13,14 +13,14 @@ function MainPage() {
     return (
         <div className="Page mainPage">
             <Logo/>
-            <img className="img2" src={img2} alt="stack of cards2"/>
+            <img className="mainPageImg2" src={img2} alt="stack of cards2"/>
             <div className="infoBox">
                 <p>Welcome to the Tiles Internet of Things Invention! <br/>
                 Pick one card from three different categories and create some smart creations.<br/>
                 Click on the button below to start!</p>
             </div>
             <LinkButton title="Let's begin!" target="/cards" onClick={() => resetCards()} size="Large"></LinkButton>
-            <img class="mainPage img1" src={img1} alt="stack of cards"/>
+            <img class="mainPageImg1" src={img1} alt="stack of cards"/>
             <div id="charNormalMainWrapper">
                 <CharNormalMainPage />
             </div>
