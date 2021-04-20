739509e7de811430c1988336d8abc300fb8f1d56
Gunvor <gunvorhu@stud.ntnu.no>
2021-04-20 10:48:28 +0200

    #38 moved component test to avoid tests from failing

--- tiles01/frontend/cypress/components/Button.spec.jsx
+++ tiles01/frontend/cypress/components/Button.spec.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { mount } from '@cypress/react'
-import FormFieldAnimationPage from '../Components/FormFieldAnimationPage';
+import FormFieldAnimationPage from '../../src/Components/FormFieldAnimationPage';
 
 
 describe('FormFieldAnimationPage', () => {
--- tiles01/frontend/src/__tests__/FormFieldAnimationPage.test.js
+++ tiles01/frontend/src/__tests__/FormFieldAnimationPage.test.js
@@ -24,35 +24,5 @@ describe('FormFieldAnimationPage component', () => {
     });
     it('renders a textarea', () => {
         expect(wrapper.find('textarea')).toHaveLength(1);
-    });
-    // it('textarea calls a function on change', () => {
-    //     const testProps = {
-    //         OnChange: jest.fn()
-    //     };
-    //     const container = shallow(<FormFieldAnimationPage {...testProps}/>);
-    //     const event = {target: {value: "sometext"}};
-    //     container.find('textarea').simulate('change', event);
-    //     expect(testProps.OnChange).toHaveBeenLastCalledWith(event);
-    // });
-
-    // it('Test click event', () => {
-    //     const mockedDownloadFile = jest.fn();
-    
-    //     const container = shallow((<FormFieldAnimationPage onChange={mockedDownloadFile}/>));
-    //     container.find('textarea').simulate('change');
-    //     expect(mockedDownloadFile.mock.calls.length).toEqual(1);
-    //   });
-
-    // it('button calls a function when clicked', () => {
-    //     const mockedDownloadFile = jest.fn();
-    //     render(<button onClick={mockedDownloadFile} className="textButtonAnimationPage">Download</button>);
-    //     userEvent.click(screen.getByRole('button'));
-    //     expect(mockedDownloadFile).toHaveBeenCalledTimes(1);
-    // });
-    // it('textarea calls a function on change', () => {
-    //     const mockedSetInput = jest.fn();
-    //     render(<textarea id="textBoxAnimationPage" placeholder="Write your idea here..." onChange={mockedSetInput} />);
-    //     userEvent.type(screen.getByRole('textbox'), 'My great idea');
-    //     expect(mockedSetInput).toHaveBeenCalledTimes(13);
-    // });  
+    }); 
 });
