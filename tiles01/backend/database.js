e603bd43b4f69e55607ad8dd94da0b19a5b0e724
stuartgo <ottersenstuart@gmail.com>
2021-04-20 08:01:20 +0200

    changed backend structure to add database test in end to end test

--- tiles01/backend/database.js
+++ tiles01/backend/database.js
@@ -22,9 +22,7 @@ async function writeIdeaDB(db_url,triggerCard, thingCard, feedbackCard, idea, re
         // Insert a single document, wait for promise so we can read it back
         const p = await ideas.insertOne(ideaDocument);
         // Find one document
-        const myDoc = await ideas.findOne({}, {sort:{$natural:-1}})
-        // Print to the console
-        console.log(myDoc,"get newest idea");
+        
 
         // res.send("Data successfully saved"); Should be added  
     }
@@ -38,29 +36,33 @@ async function writeIdeaDB(db_url,triggerCard, thingCard, feedbackCard, idea, re
         await client.close();
     }
 }
+function resolvePromise(promise){
+    return promise
+}
 
-
-async function readIdeaDB() {
-    const url = process.env.DB_URL;
+async function readIdeaDB(url) {
+    console.log(url)
     const client = new MongoClient(url,{ useUnifiedTopology: true });
-
     // The database to use
     const dbName = "tilesCards";
     try {
         await client.connect();
         console.log("Connected correctly to server");
         const db = client.db(dbName);
-
+        const ideas = db.collection("ideas");
         // Find one document
-        const myDoc = await ideas.findOne({}, {sort:{$natural:-1}})
+        const myDocPromise = await ideas.findOne({}, {sort:{$natural:-1}})
+        const myDoc = await resolvePromise(myDocPromise)
         // Print to the console
         console.log(myDoc);
-        
+        console.log("ADsd")
+        return myDoc
         // res.send("Data successfully saved"); Should be added  
     }
 
     catch (err) {
         console.log(err.stack);
+        return "jeeez"
         // res.send("----- ERROR: " + err.stack); Should be added
     }
 
