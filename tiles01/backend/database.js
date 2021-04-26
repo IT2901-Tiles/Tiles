8ff80f47195152fff8d3a94008b3accdf278010e
stuartgo <ottersenstuart@gmail.com>
2021-04-26 11:06:39 +0200

    removed poorly worded comments and strings

--- tiles01/backend/database.js
+++ tiles01/backend/database.js
@@ -18,7 +18,7 @@ async function writeIdeaDB(db_url,triggerCard, thingCard, feedbackCard, idea, re
             "cards": { "trigger": triggerCard, "thing": thingCard, "feedback": feedbackCard },
             "idea": idea
         }
-        console.log(ideaDocument);
+
         // Insert a single document, wait for promise so we can read it back
         const p = await ideas.insertOne(ideaDocument);
         // Find one document
@@ -41,7 +41,6 @@ function resolvePromise(promise){
 }
 
 async function readIdeaDB(url) {
-    console.log(url)
     const client = new MongoClient(url,{ useUnifiedTopology: true });
     // The database to use
     const dbName = "tilesCards";
@@ -54,15 +53,12 @@ async function readIdeaDB(url) {
         const myDocPromise = await ideas.findOne({}, {sort:{$natural:-1}})
         const myDoc = await resolvePromise(myDocPromise)
         // Print to the console
-        console.log(myDoc);
-        console.log("ADsd")
         return myDoc
         // res.send("Data successfully saved"); Should be added  
     }
 
     catch (err) {
         console.log(err.stack);
-        return "jeeez"
         // res.send("----- ERROR: " + err.stack); Should be added
     }
 
